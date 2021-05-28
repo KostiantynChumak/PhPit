eab706da969f84484d22fd4c53427c5c15ecd55c
Kostiantyn Chumak <60564197+KostiantynChumak@users.noreply.github.com>
2021-05-28 09:46:05 +0300

    Add files via upload

---
+++ Data/Work.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMongoDB.Data
+{
+    public class Work
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string Id { get; set; } = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+        public string Name { get; set; }
+
+        public string Roll { get; set; }
+
+        public string Age { get; set; }
+
+    }
+}
---
+++ IService/IWorkService.cs
@@ -0,0 +1,16 @@
+﻿using BlazorMongoDB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMongoDB.IService
+{
+    public interface IWorkService 
+    {
+        void SaveOrUpdate(Work work);
+        Work GetWork(string workId);
+        List<Work> GetWorks();
+        string Delete(string workId);
+    }
+}
